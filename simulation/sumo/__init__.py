f08b011ee6410a015492ce667c293abe827d43e0
liyuanxi0216@outlook.com <1236547890Wc>
2026-08-09 22:20:34 +0800

    Add init

--- simulation/sumo/__init__.py
+++ simulation/sumo/__init__.py
@@ -1,5 +1,23 @@
 """Official CityPulse SUMO signal build and runtime package."""
 
+from .algorithm import (
+    AIFrameObservation,
+    AlgorithmDecision,
+    IntersectionMetadata,
+    IntersectionObservation,
+    LaneMetadata,
+    LocalAIObserver,
+    LocalAlgorithmClient,
+    PhaseMetadata,
+    PROTOCOL_VERSION,
+    SimulationMetadata,
+    SimulationObservation,
+    SimulationTimeFrameClock,
+    VehicleAction,
+    VehicleObservation,
+    VehicleTypeMetadata,
+    to_protocol_payload,
+)
 from .building.tls import load_signal_configuration
 from .engine.signal import SafePhaseController, SignalStage
 from .engine.events import (
@@ -14,16 +32,32 @@ from .engine.distributed import RedisSimulationManager, RedisUnavailableError
 
 __all__ = [
     "AccidentEvent",
+    "AIFrameObservation",
+    "AlgorithmDecision",
+    "IntersectionMetadata",
+    "IntersectionObservation",
     "LaneClosureEvent",
+    "LaneMetadata",
+    "LocalAIObserver",
+    "LocalAlgorithmClient",
     "MajorEventClosingEvent",
     "MajorEventOpeningEvent",
     "PLAYBACK_SPEEDS",
+    "PROTOCOL_VERSION",
+    "PhaseMetadata",
     "RedisSimulationManager",
     "RedisUnavailableError",
     "SafePhaseController",
     "SignalStage",
     "SimulationConfig",
+    "SimulationMetadata",
     "SimulationManager",
+    "SimulationObservation",
+    "SimulationTimeFrameClock",
     "SpeedLimitEvent",
+    "VehicleAction",
+    "VehicleObservation",
+    "VehicleTypeMetadata",
     "load_signal_configuration",
+    "to_protocol_payload",
 ]
